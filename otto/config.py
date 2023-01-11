27a945f23dc6ad8c941c313505462b748b4f080a
zachtsk <109352381+zachtsk@users.noreply.github.com>
2023-01-11 06:53:33 -0600

    Revert config

--- otto/config.py
+++ otto/config.py
@@ -8,6 +8,35 @@ class Config:
     #########################
     # Source files
     #########################
+    data_dir: str
+    train_jsonl_fp: str
+    test_jsonl_fp: str
+
+    #########################
+    # Normalized Files
+    #########################
+    # Sample Size
+    size_sm: int
+    size_med: int
+    size_lg: int
+
+    # Output files
+    train_fp: str
+    test_fp: str
+    eval_fp: str
+    features_fp: str
+    click_embed_fp: str
+    item_embed_fp: str
+    user_embed_fp: str
+    model_fp: str
+    nn_model_fp: str
+
+
+@dataclass
+class LocalConfig:
+    #########################
+    # Source files
+    #########################
     data_dir: str = str(Path(__file__).parent.parent / "data")
     train_jsonl_fp: str = os.path.join(data_dir, "train.jsonl")
     test_jsonl_fp: str = os.path.join(data_dir, "test.jsonl")
@@ -33,13 +62,28 @@ class Config:
 
 
 @dataclass
-class LocalConfig(Config):
-    # Source/ files
-    data_dir: str = str(Path(__file__).parent.parent / "data")
-
-
-@dataclass
-class GoogleCloudConfig(Config):
+class GoogleCloudConfig:
     # Source/ files
     # TODO: change the default bucket path
     data_dir: str = os.getenv("GCP_DATA_BUCKET", "gs://otto-dataproc-gpu/otto/data")
+    train_jsonl_fp: str = os.path.join(data_dir, "train.jsonl")
+    test_jsonl_fp: str = os.path.join(data_dir, "test.jsonl")
+
+    #########################
+    # Normalized Files
+    #########################
+    # Sample Size
+    size_sm: int = 1_000
+    size_med: int = 10_000
+    size_lg: int = 100_000
+
+    # Output files
+    train_fp: str = os.path.join(data_dir, "train.parquet")
+    test_fp: str = os.path.join(data_dir, "test.parquet")
+    eval_fp: str = os.path.join(data_dir, "eval.parquet")
+    features_fp: str = os.path.join(data_dir, "features.parquet")
+    item_embed_fp: str = os.path.join(data_dir, "item_embedding.parquet")
+    user_embed_fp: str = os.path.join(data_dir, "user_embedding.parquet")
+    click_embed_fp: str = os.path.join(data_dir, "click_embedding.parquet")
+    model_fp: str = os.path.join(data_dir, "als.model")
+    nn_model_fp: str = os.path.join(data_dir, "neighbor.model")
